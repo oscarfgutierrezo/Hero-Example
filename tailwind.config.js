cc918dcbb6ec36cdc952ea8bcab8014b0e2462fc
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-03-18 15:02:58 -0500

    Add: hero-02 section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,16 @@ module.exports = {
       colors: {
         "dark-violet": "#1E0E62",
         "dark-violet-2": "#151439",
+        "light-violet": "#482BE7",
         "green": "#25DAC5",
+        "primary": "#2F1893",
       },
       fontFamily: {
         "sans": "DM Sans"
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
