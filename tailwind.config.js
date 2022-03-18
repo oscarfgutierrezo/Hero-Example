0d03547864e0b04d313cf2a7b9b3e702721634d2
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-03-18 14:21:50 -0500

    Add: hero-01 section

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "dark-violet": "#1E0E62",
+        "dark-violet-2": "#151439",
+        "green": "#25DAC5",
+      },
+      fontFamily: {
+        "sans": "DM Sans"
+      },
+    },
+  },
+  plugins: [],
+}
